4ba09c2c98a3779198ba9fdd032818b8c92bcd97
Eric Conklin <CONKLIE3@ccf.org>
2022-04-07 15:09:30 -0400

    add initial files.

---
+++ Assignment7.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Random;
+
+public class Assignment7 {
+
+    public static void main(final String[] args) throws IOException {
+        System.out.println("Hello world - this is assignment 7 in Java!");
+        System.out.println("Let's create a random list of integers up to 1000.");
+        MergeSort sort = new MergeSort();
+        System.out.println("That's all. Thanks for joining me!");
+    }
+
+}
